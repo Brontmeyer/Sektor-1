c7769135a46935e8cc6b51d06f43afbc088de107
Tyler Brontmeyer <tbrontmeyer@gmail.com>
2026-09-11 21:37:46 -0400

    Added and tested 3rd party member in battle

--- js/objects/Game_System.js
+++ js/objects/Game_System.js
@@ -4,14 +4,18 @@ class Game_System {
   constructor() {
     this.actor = new Game_Actor(1);
     this.actor2 = new Game_Actor(2);
-    
+    this.actor3 = new Game_Actor(3);
+
     // TEMP
     this.actor2.learnSkill(1);
     this.actor2.learnSkill(2);
+
+    this.actor3.learnSkill(1);
+    this.actor3.learnSkill(2);
     // TEMP
 
-    this.party = new Game_Party([this.actor, this.actor2]);
-
+    this.party = new Game_Party([this.actor, this.actor2, this.actor3]);
+    
     this.selfSwitches = new Game_SelfSwitches();
     this.switches = new Game_Switches();
 
